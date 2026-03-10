5bc688d38b1a0c8b50066ce008b50ad4427df700
Ivan Kuleshov <nirazuotslovasovsem@gmail.com>
2026-03-11 01:18:01 +0300

    Fixed the solution to the last task

--- 2026-01-13_zadacha_6/main.cpp
+++ 2026-01-13_zadacha_6/main.cpp
@@ -4,26 +4,16 @@
 int main () {
 	int number_of_array_elements;
 	std::cin >> number_of_array_elements;
+	//std::cin.ignore();
 	
 	int *array = new int[number_of_array_elements];
 	
-	std::string str;
-	std::getline(std::cin, str);
-	std::cout << str << std::endl;
-	
-	/*
 	for (int i = 0; i < number_of_array_elements; i++) {
-		for (int j = 0; j < str.length(); j++) {
-			std::cout << "!";
-		}
-		//*array + i = 
+		std::cin >> array[i];
 	}
 	
-	
-	/*
-	for (int i = 0; i < number_of_array_elements; i++) {
-		std::cout << *array + i << ' ';
+	for (int i = number_of_array_elements-1; i >= 0; i--) {
+		std::cout << array[i] << " ";
 	}
-	std::cout << std::endl; 
-	*/
+	
 }
