c5acbd17fd2815b7d6185287c8bbce98c43ed500
Ivan Kuleshov <nirazuotslovasovsem@gmail.com>
2026-07-17 22:54:02 +0300

    Added collision check

--- 2026-07-17_platformer/c_platformer/main.c
+++ 2026-07-17_platformer/c_platformer/main.c
@@ -46,9 +46,15 @@ void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeigh
 	(*obj).vertSpeed = 0;
 }
 
+bool IsCollision(TObject o1, TObject o2);
+
 void VertMoveObject(TObject *obj) {
 	(*obj).vertSpeed += 0.05;
 	SetObjectPos(obj, (*obj).x, (*obj).y + (*obj).vertSpeed);
+	if (IsCollision(*obj, brick[0])) {
+		(*obj).y -= (*obj).vertSpeed;
+		(*obj).vertSpeed = 0;
+	}
 }
 
 bool IsPosInMap(int x, int y) {
@@ -75,6 +81,11 @@ void SetCur(int x, int y) {
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+bool IsCollision(TObject o1, TObject o2) {
+	return ((o1.x + o1.width) > o2.x) && (o1.x < (o2.x + o2.width)) && 
+		   ((o1.y + o1.height) > o2.y) && (o1.y < (o2.y + o2.height));
 }
 
 int main() {
