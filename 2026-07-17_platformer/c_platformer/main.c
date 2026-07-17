3fee08b96298dfa7dace59a78373616a9f9e4b08
Ivan Kuleshov <nirazuotslovasovsem@gmail.com>
2026-07-17 21:40:40 +0300

    Added gravity and brick

--- 2026-07-17_platformer/c_platformer/main.c
+++ 2026-07-17_platformer/c_platformer/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <math.h>  
+#include <math.h>
+#include <windows.h>
 
 #define mapWidth 90
 #define mapHeight 25
@@ -9,10 +10,12 @@
 typedef struct SObject {
 	float x, y;
 	float width, height;
+	float vertSpeed;
 } TObject;
 
 char map[mapHeight][mapWidth + 1];
 TObject mario;
+TObject brick[1];
 
 void ClearMap() {
 	for (int i = 0; i < mapWidth; i++) {
@@ -40,6 +43,16 @@ void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeigh
 	SetObjectPos(obj, xPos, yPos);
 	(*obj).width = oWidth;
 	(*obj).height = oHeight;
+	(*obj).vertSpeed = 0;
+}
+
+void VertMoveObject(TObject *obj) {
+	(*obj).vertSpeed += 0.05;
+	SetObjectPos(obj, (*obj).x, (*obj).y + (*obj).vertSpeed);
+}
+
+bool IsPosInMap(int x, int y) {
+	return ((x >= 0) && (x < mapWidth) && (y >= 0) && (y < mapHeight));
 }
 
 void PutObjectOnMap(TObject obj) {
@@ -50,15 +63,35 @@ void PutObjectOnMap(TObject obj) {
 	
 	for (int i = ix; i < (ix + iWidth); i++) {
 		for (int j = iy; j < (iy + iHeight); j++) {
-			map[j][i] = '@';
+			if (IsPosInMap(i, j)) {
+				map[j][i] = '@';
+			}
 		}
 	}
 }
 
+void SetCur(int x, int y) {
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 int main() {
 	InitObject(&mario, 39, 10, 3, 3);
-	ClearMap();
-	PutObjectOnMap(mario);
-	ShowMap();
+	InitObject(brick, 20, 20, 40, 5);
+	
+	do {
+		ClearMap();
+		VertMoveObject(&mario);
+		PutObjectOnMap(brick[0]);
+		PutObjectOnMap(mario);
+		
+		SetCur(0, 0);
+		ShowMap();
+		
+		Sleep(10);
+	} while (GetKeyState(VK_ESCAPE) >= 0);
+	
 	return 0;
 }
