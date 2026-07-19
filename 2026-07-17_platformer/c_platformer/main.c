263d75e80faf968824e090b28815c3220e786af2
Ivan Kuleshov <nirazuotslovasovsem@gmail.com>
2026-07-19 21:57:26 +0300

    Added another level and the ability to move to a new level

--- 2026-07-17_platformer/c_platformer/main.c
+++ 2026-07-17_platformer/c_platformer/main.c
@@ -19,6 +19,7 @@ char map[mapHeight][mapWidth + 1];
 TObject mario;
 TObject *brick = NULL;
 int brickLength;
+int level = 1;
 
 void ClearMap() {
 	for (int i = 0; i < mapWidth; i++) {
@@ -51,7 +52,7 @@ void InitObject(TObject *obj, float xPos, float yPos, float oWidth, float oHeigh
 }
 
 bool IsCollision(TObject o1, TObject o2);
-void CreateLevel();
+void CreateLevel(int lvl);
 
 void VertMoveObject(TObject *obj) {
 	(*obj).isFly = true;
@@ -64,7 +65,9 @@ void VertMoveObject(TObject *obj) {
 			(*obj).vertSpeed = 0;
 			(*obj).isFly = false;
 			if (brick[i].cType == '+') {
-				CreateLevel();
+				level++;
+				if (level > 2) level = 1;
+				CreateLevel(level);
 				Sleep(1000);
 			}
 			break;
@@ -118,20 +121,32 @@ bool IsCollision(TObject o1, TObject o2) {
 		   ((o1.y + o1.height) > o2.y) && (o1.y < (o2.y + o2.height));
 }
 
-void CreateLevel() {
+void CreateLevel(int lvl) {
 	InitObject(&mario, 39, 10, 3, 3, '@');
-	brickLength = 6;
-	brick = realloc(brick, sizeof(*brick) * brickLength);
-	InitObject(brick + 0, 20, 20, 40, 5, '#');
-	InitObject(brick + 1, 60, 15, 10, 10, '#');
-	InitObject(brick + 2, 80, 20, 20, 5, '#');
-	InitObject(brick + 3, 120, 15, 10, 10, '#');
-	InitObject(brick + 4, 150, 20, 40, 5, '#');
-	InitObject(brick + 5, 210, 15, 10, 10, '+');
+	
+	if (lvl == 1) {
+		brickLength = 6;
+		brick = realloc(brick, sizeof(*brick) * brickLength);
+		InitObject(brick + 0, 20, 20, 40, 5, '#');
+		InitObject(brick + 1, 60, 15, 10, 10, '#');
+		InitObject(brick + 2, 80, 20, 20, 5, '#');
+		InitObject(brick + 3, 120, 15, 10, 10, '#');
+		InitObject(brick + 4, 150, 20, 40, 5, '#');
+		InitObject(brick + 5, 210, 15, 10, 10, '+');
+	}
+	if (lvl == 2) {
+		brickLength = 4;
+		brick = realloc(brick, sizeof(*brick) * brickLength);
+		InitObject(brick + 0, 20, 20, 40, 5, '#');
+		InitObject(brick + 1, 80, 20, 15, 5, '#');
+		InitObject(brick + 2, 120, 15, 15, 10, '#');
+		InitObject(brick + 3, 160, 10, 15, 15, '+');
+	}
 }
 
 int main() {
-	CreateLevel();
+	CreateLevel(level);
+	system("color 9F");
 	
 	do {
 		ClearMap();
@@ -140,7 +155,7 @@ int main() {
 		if (GetKeyState('A') < 0) HorizonMoveMap(1);
 		if (GetKeyState('D') < 0) HorizonMoveMap(-1);
 		
-		if (mario.y > mapHeight) CreateLevel();
+		if (mario.y > mapHeight) CreateLevel(level);
 		
 		VertMoveObject(&mario);
 		for (int i = 0; i < brickLength; i++){
